7fb316ce3eeb5247812f6b0fec52626a1c40c10b
Abdul Moin Qureshi <moinqureshi9707@gmail.com>
2025-04-21 17:18:44 +0500

    update

--- src/Components/Dashboard Component/Addproductcomp.jsx
+++ src/Components/Dashboard Component/Addproductcomp.jsx
@@ -1,40 +1,66 @@
 import React from 'react'
 import Inputfied from '../Common/Inputfied'
 import PrimaryBtn from '../Common/PrimaryBtn'
+import { Form, Formik } from 'formik'
 
 const Addproductcomp = () => {
+    const initialValues = {
+        Name: "",
+        Title: "",
+    }
+
+    const handleSubmit = (values) => {
+        console.log("Values");
+    }
+
     return (
         <div className='space-y-5'>
 
-            <div>
-                <Inputfied
-                    label="Product Name"
-                    id="Product Name"
-                    type="text"
-                    placeholder="Enter Product Name"
-                    name="Product Name"
-                    value=""
-                    //onChange = {}
-                    className=""
-                />
-            </div>
+            <Formik
+                initialValues={initialValues}
+                onSubmit={handleSubmit}
+            //validationSchema={validationSchema}
+            >
+                {() => {
+                    return (
+                        <Form>
+                            <div>
+                                <Inputfied
+                                    label="Product Name"
+                                    id="Name"
+                                    type="text"
+                                    placeholder="Enter Product Name"
+                                    name="Name"
+                                    value=""
+                                    //onChange = {}
+                                    className=""
+                                />
+                            </div>
 
-            <div>
-                <Inputfied
-                    label="Prodcut Title"
-                    id="Title"
-                    type="text"
-                    placeholder="Enter Product Title"
-                    name="Title"
-                    value=""
-                    //onChange = {}
-                    className=""
-                />
-            </div>
+                            <div>
+                                <Inputfied
+                                    label="Prodcut Title"
+                                    id="Title"
+                                    type="text"
+                                    placeholder="Enter Product Title"
+                                    name="Title"
+                                    value=""
+                                    //onChange = {}
+                                    className=""
+                                />
+                            </div>
 
-            <div>
-                <PrimaryBtn className="bg-green-600 hover:bg-green-500 transition-all font-semibold text-white p-2 flex justify-center items-center w-[100px] rounded-md"> Save </PrimaryBtn>
-            </div>
+                            <div>
+                                <PrimaryBtn className="bg-green-600 hover:bg-green-500 transition-all font-semibold text-white p-2 flex justify-center items-center w-[100px] rounded-md"> Save </PrimaryBtn>
+                            </div>
+
+
+                        </Form>
+                    )
+                }}
+
+            </Formik>
+
 
         </div>
     )
