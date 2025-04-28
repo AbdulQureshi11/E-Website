d05edb71106ab99712a51016e9e1c7878953b7e2
Abdul Moin Qureshi <moinqureshi9707@gmail.com>
2025-04-28 22:24:40 +0500

    creating signup page

--- src/Components/FixLayout Component/Header Component/Signup.jsx
+++ src/Components/FixLayout Component/Header Component/Signup.jsx
@@ -1,9 +1,21 @@
 import React from 'react'
+import picture from '../../../Pictures/Domore.jpg'
 
 const Signup = () => {
   return (
-    <div>
-      This is Signup Page
+    <div  className='flex justify-center absolute w-[100%]'>
+      <div>
+        <img src={picture} alt=""  />
+        </div>
+        <div className='bg-black absolute w-[100%] h-[100%] opacity-80'></div>
+
+        <div className='flex absolute w-[950px] h-[650px] rounded-3xl bg-white bg-opacity-80 mt-10'>
+          <div>
+            
+          </div>
+          <div></div>
+        </div>
+      
     </div>
   )
 }
--- src/features/counter/CategorySlice.js
+++ src/features/counter/CategorySlice.js
@@ -20,7 +20,6 @@ export const asyncGetCategories = createAsyncThunk(
         }
     }
 )
-
 export const categorySlice = createSlice({
     name: 'Category',
     initialState,
